efe7ce1c94b300357d8ebdd8522cdb0678cb3751
Christian Peralta G <makako.nessus@gmail.com>
2018-09-24 01:16:18 -0500

    Se cacha el mensaje extra desde la primera activity y se muestra en un TextView en la segunda Activity

--- app/src/main/java/unam/fca/dmoviles/SegundaActividad.java
+++ app/src/main/java/unam/fca/dmoviles/SegundaActividad.java
@@ -1,5 +1,6 @@
 package unam.fca.dmoviles;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,8 +15,19 @@ public class SegundaActividad extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_actividad);
+        // Recibir EXTRA_MESSAGE
 
         TextView tv = findViewById(R.id.tv);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if(bundle!=null){
+            String cadena_extra = (String) bundle.get("EXTRA_MESSAGE");
+            tv.setText(cadena_extra);
+
+        }
+
 
     }
 
